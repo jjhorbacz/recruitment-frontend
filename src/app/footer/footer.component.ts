a0939cf5a66ea9da8049c55c53a665c9abfbe24b
Xedilius <j.j.horbacz@gmail.com>
2024-01-27 17:37:22 +0100

    footer animation button css

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonalDataService } from './personal-data.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  rotationAngle: number = 45;
+  mbValue: string = 'auto';
+  isClicked: boolean = false;
+  enablePersonalData: boolean = false;
+  resetDataStatus: boolean = true;
 
+  constructor(private personalDataService: PersonalDataService) { }
+
+  ngOnInit(): void {
+  }
+
+  toggleFrame() {
+    this.isClicked = !this.isClicked;
+    if (this.isClicked) {
+      this.rotationAngle = 225;
+      this.mbValue = '3px';
+    } else {
+      this.rotationAngle = 45;
+      this.mbValue = 'auto';
+    }
+  }
+
+  showUserData() {
+    this.enablePersonalData = true;
+    this.personalDataService.setDataStatus(this.enablePersonalData);
+    this.toggleFrame();
+  }
+
+  resetSettings() {
+    this.enablePersonalData = false;
+    this.personalDataService.setDataStatus(this.enablePersonalData);
+    this.personalDataService.setResetStatus(this.resetDataStatus);
+    this.toggleFrame();
+  }
 }
